b2bb34f834ceee51b1dbf1de57fed71f35da8733
Shuo Chen <nebaries@gmail.com>
2013-06-25 10:25:34 +0100

    style changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def main_content_class
+    if content_for?(:left_sidebar) && content_for?(:right_sidebar)
+      'span6'
+    elsif content_for?(:left_sidebar)
+      'span9 left-bordered'
+    elsif content_for?(:right_sidebar)
+      'span9 right-bordered'
+    else
+      'span12'
+    end
+  end
+  
+  def display_shoutbox_and_recruitment
+    content_for(:left_sidebar, render('shoutbox')) 
+    content_for(:right_sidebar, render('recruitment')) 
+  end
 end
