161547670da472343a3487244ddedb2c95c09027
Shuo Chen <nebaries@gmail.com>
2013-06-27 00:04:10 +0100

    removed the database config from repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,17 +20,17 @@ set :user, 'rails'
 server '31.222.164.148', :app, :web, :db, :primary => true
 
 namespace :deploy do
-  # namespace :assets do
-  #     task :precompile, :roles => :web, :except => { :no_release => true } do
-  #       from = source.next_revision(current_revision)
-  #       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #         run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
-  #       else
-  #         logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #       end
-  #     end
-  #   end
-  #   
+  namespace :assets do
+      task :precompile, :roles => :web, :except => { :no_release => true } do
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+      end
+    end
+    
   task :start, :roles => :app do
     run "cd #{current_release}; script/server start"
   end
