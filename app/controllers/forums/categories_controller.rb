1783d1613f9744886c7139566b2b6d1ae25b8c25
Shuo Chen <nebaries@gmail.com>
2013-06-30 23:22:54 +0100

    oops

--- app/controllers/forums/categories_controller.rb
+++ app/controllers/forums/categories_controller.rb
@@ -11,7 +11,7 @@ module Forums
     
     private
       def find_category
-        @category = Category.includes(boards: [:viewable, :posts]).find(params[:id]) if params[:id]
+        @category = Category.includes(boards: [:views, :posts]).find(params[:id]) if params[:id]
       end
   end
 end
