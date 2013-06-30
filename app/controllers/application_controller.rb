4c972d4c3acb11d18c1ac52e0ce4f2064d96c9b7
Shuo Chen <nebaries@gmail.com>
2013-06-30 13:47:57 +0100

    whos online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :set_header  
-  helper_method :all_pages
+  before_filter :set_current_online_users
+  helper_method :all_pages, :current_online_users
   
   def all_pages
     @all_pages ||= Page.ordered.decorate
   end
   
   ## The following are used by our Responder service classes so we can access
-  ## the instance variable for the current resource easily via a standard method
+  ## the instance variable for the current resource easily via a standard method  
   def resource_name
     controller_name.demodulize.singularize
   end
@@ -22,8 +23,29 @@ class ApplicationController < ActionController::Base
     instance_variable_set(:"@#{resource_name}", val)
   end
   
+  def current_online_users
+    @current_online_users ||= get_current_online_users
+  end
+  
+  
   private
     def set_header
       response.headers['X-XSS-Protection'] = "0"
+    end
+    
+    def set_current_online_users
+      return unless user_signed_in?
+      online_users = Rails.cache.fetch('online_users') do
+        {}
+      end
+      online_users[current_user.id] = Time.now.utc
+      Rails.cache.write('online_users', online_users)
+    end    
+    
+    def get_current_online_users
+      online_users = Rails.cache.fetch('online_users') do
+        {}
+      end
+      User.where(id: online_users.select{ |_, timestamp| timestamp > 5.minutes.ago }.keys).decorate
     end
 end
