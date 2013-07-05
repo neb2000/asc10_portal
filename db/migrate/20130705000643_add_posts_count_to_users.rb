2feb07b927e7f86300884055b75f718119ae1edb
Shuo Chen <nebaries@gmail.com>
2013-07-05 02:03:39 +0100

    counter cache migration

--- db/migrate/20130705000643_add_posts_count_to_users.rb
+++ db/migrate/20130705000643_add_posts_count_to_users.rb
@@ -2,7 +2,7 @@ class AddPostsCountToUsers < ActiveRecord::Migration
   def change
     add_column :users, :posts_count, :integer, default: 0
     User.find_each do |user|
-      user.update_column(:posts_count, user.posts.length)
+      User.where(id: user.id).update_all(:posts_count, user.posts.length)
     end
   end
 end
