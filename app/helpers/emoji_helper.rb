f42f96b8a9af6936104b8737f0c597cf49c3fda9
Shuo Chen <nebaries@gmail.com>
2013-07-06 04:17:42 +0100

    image size

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -2,7 +2,7 @@ module EmojiHelper
  def emojify(content)
     h(content).to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
       if Emoji.names.include?($1)
-        '<img alt="' + $1 + '" height="20" src="/images/emoji/' + "#{$1}" +'.png" style="vertical-align:middle" width="20" />'
+        '<img alt="' + $1 + '" src="/images/emoji/' + "#{$1}" +'.png" style="vertical-align:middle" width="20" height="20" />'
       else
         match
       end
