9672b1a48946cdc1efa118fc053f3e5e66aee8b8
Shuo Chen <nebaries@gmail.com>
2013-07-17 01:23:58 +0100

    uploader for cover image

--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -21,11 +21,11 @@ class CoverImageUploader < CarrierWave::Uploader::Base
   end
 
   version :large do
-    process resize_to_fill: [650, 325]
+    process resize_to_fill: [496, 248]
   end
   
   version :small do
-    process resize_to_fill: [324, 162]
+    process resize_to_fill: [234, 117]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
