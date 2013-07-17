0796aafa7d6b1e65bd0f7112d73f3824df011846
Shuo Chen <nebaries@gmail.com>
2013-07-17 01:43:25 +0100

    added map file for jquery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Asc10Portal::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( admin.css admin.js wysiwyg-editor.css )
+  config.assets.precompile += %w( admin.css admin.js wysiwyg-editor.css jquery-2.0.3.min.map )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
